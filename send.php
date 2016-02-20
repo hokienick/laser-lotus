24668c691f916f247a85dee40d07eee0805d777b
Nick Castillo <nickcastillo@Nicks-MacBook-Pro.local>
2016-02-19 19:10:40 -0800

    Go back to Original Plan part 3

--- send.php
+++ send.php
@@ -1,6 +1,7 @@
 <?php
+require 'PHPMailerAutoload.php';
 if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email']) && isset($_POST['message'])) {
-	require("PHPMailer-master/class.phpmailer.php"); //you have to download this plugin from github.com - link is in documentation
+	//require("PHPMailer-master/class.phpmailer.php"); //you have to download this plugin from github.com - link is in documentation
 	
 	$firstname = 	$_POST['firstname'];
 	$lastname = 	$_POST['lastname'];
@@ -8,16 +9,52 @@ if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['emai
 	$message = 		$_POST['message'];
 	
 	$mail = new PHPMailer();
-	
+
+	//Tell PHPMailer to use SMTP
+	$mail->isSMTP();
+
+	//Enable SMTP debugging
+	// 0 = off (for production use)
+	// 1 = client messages
+	// 2 = client and server messages
+	$mail->SMTPDebug = 2;
+
+	//Ask for HTML-friendly debug output
+	$mail->Debugoutput = 'html';
+
+	//Set the hostname of the mail server
+	$mail->Host = 'smtp.gmail.com';
+	// use
+	// $mail->Host = gethostbyname('smtp.gmail.com');
+	// if your network does not support SMTP over IPv6
+
+	//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+	$mail->Port = 587;
+
+	//Set the encryption system to use - ssl (deprecated) or tls
+	$mail->SMTPSecure = 'tls';
+
+	//Whether to use SMTP authentication
+	$mail->SMTPAuth = true;
+
+	//Username to use for SMTP authentication - use full email address for gmail
+	$mail->Username = "nicastil@vt.edu";
+
+	//Password to use for SMTP authentication
+	$mail->Password = "Nachos11!";
+
+
+	//formulate email
 	$mail->From = $email;
 	$mail->CharSet = "UTF-8";
 	$mail->FromName = $email;
 	$mail->addAddress("hokienick@gmail.com"); 
 	
 	$mail->IsHTML(true);
-	$mail->Subject = $name." sent you an email";
+	$name = $firstname . $lastname
+	$mail->Subject = $name." sent you an email from your website";
 	
-	$mail->Body = $message."<br><br>".$name."<br>".$email."<br>".$phone."<br>";
+	$mail->Body = $message."<br><br>".$name."<br>".$email."<br>";
 
 	$mail->send();
 }
