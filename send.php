738cc15b664129dca65c05418ece544e12dcbcd9
Nick Castillo <nickcastillo@Nicks-MBP.fios-router.home>
2016-02-21 15:42:52 -0500

    this is never gonna work

--- send.php
+++ send.php
@@ -1,7 +1,7 @@
 
 <?php
 if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email']) && isset($_POST['message'])) {
-	require("PHPMailer-master/class.phpmailer.php"); //you have to download this plugin from github.com - link is in documentation
+	require 'PHPMailer-master/PHPMailerAutoload.php'; //you have to download this plugin from github.com - link is in documentation
 	
 	$firstname = 	$_POST['firstname'];
 	$lastname = 	$_POST['lastname'];
@@ -9,6 +9,38 @@ if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['emai
 	$message = 		$_POST['message'];
 	
 	$mail = new PHPMailer();
+
+	$mail->isSMTP();
+
+	//Enable SMTP debugging
+	// 0 = off (for production use)
+	// 1 = client messages
+	// 2 = client and server messages
+	$mail->SMTPDebug = 2;
+
+	//Ask for HTML-friendly debug output
+	$mail->Debugoutput = 'html';
+
+	//Set the hostname of the mail server
+	$mail->Host = 'smtp.gmail.com';
+	// use
+	// $mail->Host = gethostbyname('smtp.gmail.com');
+	// if your network does not support SMTP over IPv6
+
+	//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+	$mail->Port = 587;
+
+	//Set the encryption system to use - ssl (deprecated) or tls
+	$mail->SMTPSecure = 'tls';
+
+	//Whether to use SMTP authentication
+	$mail->SMTPAuth = true;
+
+	//Username to use for SMTP authentication - use full email address for gmail
+	$mail->Username = "laserlotusproductions@google.com";
+
+	//Password to use for SMTP authentication
+	$mail->Password = "Nachos11!";
 	
 	$mail->From = $email;
 	$mail->CharSet = "UTF-8";
