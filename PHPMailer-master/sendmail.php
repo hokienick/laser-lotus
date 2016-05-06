63c3e85a9e706e24f65bc3706da10aaf43dcf562
Nick Castillo <nickcastillo@Nicks-MBP.fios-router.home>
2016-05-06 13:53:37 -0400

    more mail shenanigans

--- PHPMailer-master/sendmail.php
+++ PHPMailer-master/sendmail.php
@@ -1,41 +1,34 @@
+<?php 
 
-<?php
-if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email']) && isset($_POST['message'])) {
-	require 'PHPMailer-master/PHPMailerAutoload.php'; //you have to download this plugin from github.com - link is in documentation
-	include 'ChromePhp.php';
-	
-	$firstname = 	$_POST['firstname'];
-	$lastname = 	$_POST['lastname'];
-	$email = 		$_POST['email'];
-	$message = 		$_POST['message'];
-	
-	$mail = new PHPMailer();  // create a new object
-	$mail->IsSMTP(); // enable SMTP
-	$mail->SMTPDebug = 3;  // debugging: 1 = errors and messages, 2 = messages only
-	$mail->SMTPAuth = true;  // authentication enabled
-	$mail->SMTPSecure = 'ssl'; //Set the encryption system to use - ssl (deprecated) or tls
-	$mail->Host = 'smtp.gmail.com';
-	$mail->Port = 465; 
-	$mail->Username = 'laserlotusproductions@gmail.com';  
-	$mail->Password = 'Nachos11!';          
-	$mail->From = $email;
-	$name = $firstname . " " . $lastname;
-	ChromePhp::log("added name: ",$name);
-	$mail->Subject = $name." sent you an email";
-	ChromePhp::log("added subject");
-	$mail->Body = $message."<br><br>".$name."<br>".$email."<br>".$phone."<br>";
-	ChromePhp::log("Added Body");
-	$mail->addAddress("hokienick@gmail.com"); 
-	ChromePhp::log("Added Address and about to send");
-
-	if(!$mail->Send()) {
-		ChromePhp::log("Mail error: ", $mail->ErrorInfo);
-		return false;
-	} else {
-		ChromePhp::log("Message seemed to have sent fine");
-		$error = 'Message sent!';
-		return true;
-	}
-	ChromePhp::log("Did we make it to the end?");
-}
-?>  
+ require_once('class.phpmailer.php');
+ 
+    $mail = new PHPMailer();
+    $mail->CharSet =  "utf-8";
+    $mail->IsSMTP();
+    $mail->SMTPAuth = true;
+    $mail->Username = "hokienick@gmail.com";
+    $mail->Password = "7AYF#eVO#UAIuZ";
+    $mail->SMTPSecure = "ssl";  
+    $mail->Host = "smtp.gmail.com";
+    $mail->Port = "465";
+ 
+    $mail->setFrom('hokienick@gmail.com', 'Nick Castillo');
+ 
+    $mail->Subject  =  'Mail from your website';
+    $mail->IsHTML(true);
+    $mail->Body    = 'Hi there ,
+                        <br />
+                        this mail was sent using PHPMailer...
+                        <br />
+                        cheers... :)';
+  
+     if($mail->Send())
+     {
+        echo "Message was Successfully Sent :)";
+     }
+     else
+     {
+        echo "Mail Error - >".$mail->ErrorInfo;
+     }
+  
+?>
